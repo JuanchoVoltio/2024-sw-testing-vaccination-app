9914d58df9aa4b595b8a8c6fb5f5ec822e197ec2
Sebastián martin <sebasmartin930@gmail.com>
2024-10-22 01:13:18 -0500

    Se hizo corrección del código y testing además se incluyó Spring Boot

--- src/main/java/edu/softwaretesting2/dummymavenapp/vaccination_app/HelloController.java
+++ src/main/java/edu/softwaretesting2/dummymavenapp/vaccination_app/HelloController.java
@@ -1,6 +1,5 @@
 package edu.softwaretesting2.dummymavenapp.vaccination_app;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,13 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     private final HelloService service;
-    @Autowired
-    HelloController(HelloService service){
+
+    // Constructor para la inyección de dependencias del servicio
+    public HelloController(HelloService service) {
         this.service = service;
     }
 
-    @GetMapping
-    String sayHello(){
+    // Definir una ruta explícita para este controlador
+    @GetMapping("/hello")
+    public String sayHello() {
         return service.sayHello("ES") + "\n";
     }
 }
