58d1297a0ee8f70d3e24f15dacf2d31faae6da9a
Laura valentina segura carrasquilla <lvfc1420@gmail.com>
2024-11-18 23:26:38 -0500

    Update VacunacionServiceTest.java

--- src/test/java/edu/softwaretesting2/dummymavenapp/vaccination_app/service/VacunacionServiceTest.java
+++ src/test/java/edu/softwaretesting2/dummymavenapp/vaccination_app/service/VacunacionServiceTest.java
@@ -3,8 +3,9 @@ package edu.softwaretesting2.dummymavenapp.vaccination_app.service;
 import edu.softwaretesting2.dummymavenapp.vaccination_app.model.Paciente;
 import org.junit.jupiter.api.*;
 
-import javax.lang.model.util.AbstractAnnotationValueVisitor6;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class VacunacionServiceTest {
@@ -12,37 +13,50 @@ class VacunacionServiceTest {
     Paciente persona1;
     Paciente personaPrioritaria;
     boolean respuesta;
+    VacunacionService servicio;
 
     @BeforeAll
-    void init(){
+    void init() {
         persona1 = new Paciente("NN", LocalDate.of(1986, 12, 24));
         personaPrioritaria = new Paciente("Bla", LocalDate.of(1963, 12, 24));
+        servicio = new VacunacionService();
     }
 
     @BeforeEach
     void setUp() {
+       
     }
 
     @Test
-    public void deberíaAsignarMaximo40PersonasParaUnDía(){
-        //GIVEN
+    public void deberíaAsignarMaximo40PersonasParaUnDía() {
+        // GIVEN
+        List<Paciente> pacientes = generarPacientes(50); // Generamos 50 pacientes
 
-        //WHEN
+        // WHEN
+        List<Paciente> pacientesAsignados = servicio.asignarCitasDiarias(pacientes);
 
-        //THEN
-
+        // THEN
+        Assertions.assertEquals(40, pacientesAsignados.size(), "Debe asignar máximo 40 personas por día");
     }
 
     @Test
-    public void deberíaDefinirComoPriotitariaAUnaPersonaDeMasDe60Años(){
-        //GIVEN
+    public void deberíaDefinirComoPriotitariaAUnaPersonaDeMasDe60Años() {
+        // GIVEN
         LocalDate fechaActual = LocalDate.now();
-        VacunacionService servicio = new VacunacionService();
 
-        //WHEN
+        // WHEN
         boolean respuesta = servicio.validarLaEdadDelPaciente(personaPrioritaria);
 
-        //THEN
-        Assertions.assertTrue(respuesta);
+        // THEN
+        Assertions.assertTrue(respuesta, "Una persona mayor de 60 años debe ser prioritaria");
     }
+
+    // Método para generar una lista de pacientes
+    private List<Paciente> generarPacientes(int cantidad) {
+        List<Paciente> pacientes = new ArrayList<>();
+        for (int i = 0; i < cantidad; i++) {
+            pacientes.add(new Paciente("Paciente" + i, LocalDate.of(1990, 1, 1).plusDays(i)));
+        }
+        return pacientes;
+    }
 }
